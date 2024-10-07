533e199b303c5991d468a2821434146b32e99eef
hzrd149 <github@hzrd149.com>
2024-10-07 11:56:38 -0500

    fix entrypoint

--- scripts/procedures/main.ts
+++ scripts/procedures/main.ts
@@ -2,6 +2,6 @@ import { types as T, util } from "../deps.ts";
 
 export const main = async (effects: T.Effects) => {
   // args defaulted to [] - not necessary to include if empty
-  await effects.runDaemon({ command: "docker_entrypoint.sh", args: [] }).wait();
+  await effects.runDaemon({ command: "docker-entrypoint.sh", args: [] }).wait();
   return util.ok;
 };
